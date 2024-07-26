7c8218ef13e0207ae828da89ab65e3d2e6adc75b
willculkin <wicu6711@colorado.edu>
2024-07-26 12:25:35 +0100

    initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Routes,
+} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+    <Routes>
+      <Route  path={ROUTES.LANDING} Component={LandingPage} />
+      <Route  path={ROUTES.SIGN_UP} Component={SignUpPage} />
+      <Route  path={ROUTES.SIGN_IN} Component={SignInPage} />
+      <Route
+
+        path={ROUTES.PASSWORD_FORGET}
+        Component={PasswordForgetPage}
+      />
+      <Route  path={ROUTES.HOME} Component={HomePage} />
+      <Route  path={ROUTES.ACCOUNT} Component={AccountPage} />
+      <Route  path={ROUTES.ADMIN} Component={AdminPage} />
+       </Routes>
+    </div>
+  </Router>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,3 +19,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorker.unregister();
+
+
+
